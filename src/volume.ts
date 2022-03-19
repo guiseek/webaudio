7bada952f0aafb50a212bc637b37f07cff11f894
Gui Seek <guiseek@gmail.com>
2022-03-19 18:45:23 -0300

    exemplo volume

--- src/volume.ts
+++ src/volume.ts
@@ -2,3 +2,21 @@ import './guiseek'
 
 import './style.scss'
 
+
+const audio = document.querySelector('audio')!;
+const form = document.querySelector('form')!;
+const volumeControl = document.querySelector('input')!;
+const volumeValue = document.querySelector('output')!;
+
+
+function changeVolume(value: number) {
+  audio.volume = value / 100;
+  volumeValue.innerHTML = `${value}`;
+}
+
+
+changeVolume(volumeControl.valueAsNumber);
+
+volumeControl.oninput = () => changeVolume(volumeControl.valueAsNumber);
+
+form.onreset = () => changeVolume(50);
